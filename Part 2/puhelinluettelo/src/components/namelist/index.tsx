ff5abe225e4a2b76535579c90e02f25e07b8df2d
emsknock <emsknock@users.noreply.github.com>
2020-02-02 19:28:53 +0200

    Add key to each name

--- Part 2/puhelinluettelo/src/components/namelist/index.tsx
+++ Part 2/puhelinluettelo/src/components/namelist/index.tsx
@@ -2,5 +2,5 @@ import React, { FC } from "react";
 import { I_Person } from "../../types/person";
 
 export const Namelist: FC<{ persons: I_Person[] }> = ({ persons }) => <div>
-    {persons.map(p => <div>{p.name} • {p.phone}</div>)}
+    {persons.map(p => <div key={p.name}>{p.name} • {p.phone}</div>)}
 </div>;
