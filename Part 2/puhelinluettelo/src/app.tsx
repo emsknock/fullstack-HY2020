8507f6b086a4ada32ae641652a499add4de7664c
emsknock <emsknock@users.noreply.github.com>
2020-02-02 21:16:45 +0200

    Complete exercises 2.19–2.20

--- Part 2/puhelinluettelo/src/app.tsx
+++ Part 2/puhelinluettelo/src/app.tsx
@@ -5,11 +5,14 @@ import { I_Person } from "./types/person";
 import { Namelist } from "./components/namelist";
 import { NewNameForm } from "./components/newname-form";
 import { FilterForm } from "./components/filter-form";
+import { Toaster } from "./components/toaster";
 
 export const App: FC = () => {
 
     const [persons, setPersons] = useState<I_Person[]>([]);
     const [filter, setFilter] = useState("");
+    const [success, setSuccess] = useState<string | null>(null);
+    const [error, setError] = useState<string | null>(null);
 
     const personsToShow = persons.filter(p => p.name.toLowerCase().includes(filter));
 
@@ -20,23 +23,40 @@ export const App: FC = () => {
         const collision = persons.find(p => p.name === newPerson.name);
 
         if (!collision)
-            return db.newPerson(newPerson).then(p => setPersons(s => s.concat(p)));
+            return db.newPerson(newPerson).then(p => {
+                setPersons(s => s.concat(p));
+                setSuccess(`${p.name} added`);
+                setTimeout(() => setSuccess(null), 3000);
+            });
 
         if (!window.confirm(`${newPerson.name} already exists. Update number?`))
             return;
 
         db.updatePerson({ ...newPerson, id: collision.id })
-            .then(n => setPersons(persons.map(p => p.id === n.id ? n : p)));
-        
+            .then(n => {
+                setPersons(persons.map(p => p.id === n.id ? n : p));
+                setSuccess(`${n.name} updated`);
+                setTimeout(() => setSuccess(null), 3000);
+            })
+            .catch(_ => {
+                setError(`${newPerson.name} already removed from server`);
+                setTimeout(() => setError(null), 3000);
+            })
+
     };
 
     const onRemovePerson = (person: I_Person) =>
-        db.removePerson(person).then(hydratePersonList);
+        db.removePerson(person).then(_ => {
+            hydratePersonList();
+            setSuccess(`${person.name} removed`);
+            setTimeout(() => setSuccess(null), 3000);
+        });
 
     useEffect(() => void hydratePersonList(), []);
 
     return <div>
         <h2>Phonebook</h2>
+        <Toaster {...{ success, error }} />
         <FilterForm value={filter} onChange={setFilter} />
         <h2>Add name</h2>
         <NewNameForm onNewName={onNewName} />
