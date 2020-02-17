b5b0f36fc497bb4241850d13e904797feae92e81
emsknock <emsknock@users.noreply.github.com>
2020-02-17 20:29:37 +0200

    Add test db url

--- Part 4/blogilista/utils/config.ts
+++ Part 4/blogilista/utils/config.ts
@@ -1,6 +1,14 @@
+import { error } from "./logger";
+
 require("dotenv").config();
 
 export const PORT = process.env.PORT ?? 3003;
 
 export const DB_PASS = process.env.DB_PASS;
-export const MONGO_URL = `mongodb+srv://fullstack:${DB_PASS}@cluster0-x5cql.mongodb.net/test?retryWrites=true&w=majority`;
+if (!DB_PASS) {
+    error("No DB_PASS variable set!");
+    process.exit(1);
+}
+
+export const MONGO_URL =
+    `mongodb+srv://fullstack:${DB_PASS}@cluster0-x5cql.mongodb.net/blog${process.env.NODE_ENV === "test" ? "-test" : ""}?retryWrites=true&w=majority`;
