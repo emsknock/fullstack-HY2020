e0edba6b80eb81b4934316a059cf75c61d9635a9
emsknock <emsknock@users.noreply.github.com>
2020-02-17 20:35:17 +0200

    Only log info when not in test env

--- Part 4/blogilista/utils/logger.ts
+++ Part 4/blogilista/utils/logger.ts
@@ -1,2 +1,5 @@
-export const info = (...data: any[]) => console.info(...data);
+export const info = (...data: any[]) => {
+    if (process.env.NODE_ENV !== "test")
+        console.info(...data);
+}
 export const error = (...data: any[]) => console.error(...data);
