df8114cbd3f133279ad7553183adde2f5e073d8d
emsknock <emsknock@users.noreply.github.com>
2020-02-18 18:55:12 +0200

    Formatting

--- Part 4/blogilista/app.ts
+++ Part 4/blogilista/app.ts
@@ -3,9 +3,10 @@ import { MONGO_URL } from "./utils/config";
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+
 import { blogsRouter } from "./controllers/blogs";
-import { errorHandler } from "./controllers/errors";
 import { usersRouter } from "./controllers/users";
+import { errorHandler } from "./controllers/errors";
 
 require("express-async-errors");
 
@@ -22,8 +23,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/blogs", blogsRouter);
-app.use("/api/users", usersRouter)
+app.use("/api/users", usersRouter);
 
 app.use(errorHandler);
 
