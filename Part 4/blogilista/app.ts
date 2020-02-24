4df3c067ee5ea8f3715c75acac1fa97c09a0e1ca
emsknock <emsknock@users.noreply.github.com>
2020-02-24 17:07:01 +0200

    Do not use token middleware on login router

--- Part 4/blogilista/app.ts
+++ Part 4/blogilista/app.ts
@@ -25,11 +25,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/login", loginRouter);
+
 app.use(tokenExtractor);
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
-app.use("/api/login", loginRouter);
 
 app.use(errorHandler);
 
