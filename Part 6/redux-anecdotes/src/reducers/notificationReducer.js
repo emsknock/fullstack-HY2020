2be6aa8fdd1d0ebcf025aea561af836252cf4be4
emsknock <emsknock@users.noreply.github.com>
2020-03-02 19:38:20 +0200

    Fix notification disappearing prematurely

--- Part 6/redux-anecdotes/src/reducers/notificationReducer.js
+++ Part 6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -3,7 +3,7 @@ const defaultState = "";
 export const setNotification = text => async dispatch => {
     dispatch({ type: "SET_NOTIFICATION", text });
     setTimeout(
-        () => dispatch({ type: "SET_NOTIFICATION", text: "" }),
+        () => dispatch({ type: "CLEAR_NOTIFICATION", text }),
         5000
     );
 }
@@ -12,6 +12,11 @@ export default (state = defaultState, action) => {
 
     if (action.type === "SET_NOTIFICATION")
         return action.text;
+    if (action.type === "CLEAR_NOTIFICATION")
+        // Old timeouts might try to clear text that has since changed,
+        // so we only clear the text if the timeout matches the current notification
+        if (state === action.text)
+            return "";
 
     return state;
 
